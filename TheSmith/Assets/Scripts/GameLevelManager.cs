133c67311298c0c6dc18dce6f9c2d0182d192e1b
sumetx <sumet@Sumets-MacBook-Air.local>
2016-03-07 09:37:26 +0700

    Update Project
    There is not thing new.

---
+++ TheSmith/Assets/Scripts/GameLevelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameLevelManager : MonoBehaviour {
+
+	public void gotoShop()
+	{
+		SceneManager.LoadScene("Shop");
+	}
+
+	public void gotoAdventure()
+	{
+		SceneManager.LoadScene("Adventure");
+	}
+
+	public void gotoMain()
+	{
+		SceneManager.LoadScene("GameLevel1");
+	}
+
+	public Image dayTime;
+	public Image NightTime;
+	public Text timeTextUI;
+	public Slider expSliderUI;
+	int expValue;
+
+	void Update()
+	{
+		if( Input.GetKey(KeyCode.Space) )
+		{
+			dayTime.enabled = false;
+			NightTime.enabled = true;
+			expValue++;
+			expSliderUI.value = expValue; 
+		}
+		else
+		{
+			dayTime.enabled = true;
+			NightTime.enabled = false;
+			//expValue--;
+			//expSliderUI.value = expValue;
+		}
+
+		timeTextUI.text = Time.deltaTime.ToString();
+	}
+}
