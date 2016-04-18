4c1e9dcd959c2010d88438e8c1d994ae41c393ca
sumetx <sumet@Sumets-MacBook-Air.local>
2016-04-18 08:36:51 +0700

    Game Project Update Unity version 5.4.3
    Update Unity engine to version 5.4.3f

---
+++ TheSmith/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float leftRight = Input.GetAxis("Horizontal");
+		transform.Translate(leftRight*Time.deltaTime,0,0);
+	
+	}
+}
